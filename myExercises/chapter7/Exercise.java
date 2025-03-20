bb324247447c6ec0ff1f1b99dbbf7936786b3564
vinelis <lucavinelli4@gmail.com>
2025-03-20 15:27:43 -0300

    Exercisesfive

---
+++ myExercises/chapter7/Exercise.java
@@ -0,0 +1,38 @@
+package myExercises.chapter7;
+
+import java.util.Scanner;
+
+public class Exercise {
+
+    private static final String[] DAYSOFTHEWEEK = {"Mondey", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday"};
+    private static int number;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Give me a number an i will give you a day of the week ");
+        number = scanner.nextInt();
+        scanner.close();
+
+
+    }
+
+    public static int lenghtArray(String[] array){
+        int out;
+        int i = 0;
+
+        while(){
+
+        }
+    }
+
+
+    /*
+    public static String dayByNumber(String[] array, int number){
+
+        for(int i =0, i)
+    }
+     */
+
+}
+
